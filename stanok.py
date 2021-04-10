902c8a807a647065196c90372e02d5fa91e1f242
fedorovnvkotleta <82046502+fedorovnvkotleta@users.noreply.github.com>
2021-04-10 15:51:57 +0300

    Update stanok.py

--- stanok.py
+++ stanok.py
@@ -1,9 +1,9 @@
 from .. import loader
 from asyncio import sleep
-@wenryftg
+@loader.tds
 class StanokMod(loader.Module):
 	strings = {"name": "stanok"}
-	@wenryftg
+	@loader.owner
 	async def wenrycmd(self, message):
 		for _ in range(10):
 			for heart in ['венри', '️хелси', 'норка', 'боуз', 'молик', 'вейв', 'прокод', 'ачё', 'ладскер', 'фантик', 'заново ебать']:
