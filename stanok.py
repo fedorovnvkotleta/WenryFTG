36c56c1b2eaa32ad99ea23fc4921ca84d1566811
fedorovnvkotleta <82046502+fedorovnvkotleta@users.noreply.github.com>
2021-04-10 15:50:11 +0300

    Create stanok.py

---
+++ stanok.py
@@ -0,0 +1,11 @@
+from .. import loader
+from asyncio import sleep
+@wenryftg
+class StanokMod(loader.Module):
+	strings = {"name": "stanok"}
+	@wenryftg
+	async def wenrycmd(self, message):
+		for _ in range(10):
+			for heart in ['венри', '️хелси', 'норка', 'боуз', 'молик', 'вейв', 'прокод', 'ачё', 'ладскер', 'фантик', 'заново ебать']:
+				await message.edit(stanok)
+				await sleep(0.2)
